890ec27c73b230e704b8b987a7fdea929a7b566e
Puneet Tripathi <31759593+puneettripathi@users.noreply.github.com>
2018-03-04 15:11:01 +0530

    Update PT_timeseries_with_DataChecks.R

--- PT_timeseries_with_DataChecks.R
+++ PT_timeseries_with_DataChecks.R
@@ -1,6 +1,6 @@
 #Loading necessary libraries
 # Keep next line commented, for dev purpose only
-# setwd("C:/Users/putripat/Downloads/TimeSeries")
+# setwd("D:/pgdds/Time Series/TimeSeries/")
 # Run these if you get package not found error
 # install.packages('forecast')
 # install.packages('tseries')
@@ -11,7 +11,7 @@ library(forecast)
 library(tseries)
 require(graphics)
 
-##DATA PREPARATION
+###### DATA PREPARATION #######
 
 #Reading the data from CSV file
 Data <- read.csv("Global Superstore.csv")
@@ -34,6 +34,19 @@ str(Data)
 colnames(Data)[colSums(is.na(Data)) > 0]
 # "Postal.Code"
 # Only one column Postal.Code has missing values
+
+# Checking for columns with same values
+sapply(Data,function(x) length(unique(x)))
+# No column with just 1 value
+# Row.ID       Order.ID     Order.Date      Ship.Date      Ship.Mode    Customer.ID  Customer.Name 
+# 51290          25035           1430           1464              4           1590            795 
+# Segment           City          State        Country    Postal.Code         Market         Region 
+# 3           3636           1094            147            632              7             13 
+# Product.ID       Category   Sub.Category   Product.Name          Sales       Quantity       Discount 
+# 10292              3             17           3788          22995             14             27 
+# Profit  Shipping.Cost Order.Priority    Order.Month     Order.Year 
+# 24575          10037              4             12              4 
+
 
 #Extracing month and year from Order Date for aggregation and sorting
 Data$Order.Month <- substr(Data$Order.Date, 4, 5)
@@ -175,9 +188,8 @@ class_dec_pred <- c(ts(global_pred),ts(global_pred_out))
 plot(total_timeser, col = "black")
 lines(class_dec_pred, col = "red")
 
-# Forecasting future months
+# Forecasting future months Using Classical Decomposition
 forecast.decompose <- predict(lmfit,data.frame(Month =49:54))
-line(forecast.decompose)
 
 ######## AUTO ARIMA ########
 #So, that was classical decomposition, now let's do an ARIMA fit
@@ -223,7 +235,7 @@ plot(forecast(auto_arima_pred, h=12))
 
 
 
-
+####### APAC Demand Modeling #######
 #For Forecasting Demand(Quantity) for APAC Consumer Segment
 Data_ts <- Data_AC_Quantity
 names(Data_ts) <- c("Month", "Value")
@@ -293,10 +305,10 @@ armafit
 #We'll check if the residual series is white noise
 resi <- local_pred-fitted(armafit)
 adf.test(resi,alternative = "stationary")
-#Dickey-Fuller = -5.1426, Lag order = 3, p-value = 0.01
+# Dickey-Fuller = -5.0179, Lag order = 3, p-value = 0.01
 #alternative hypothesis: stationary
 kpss.test(resi)
-#KPSS Level = 0.038123, Truncation lag parameter = 1, p-value = 0.1
+# KPSS Level = 0.028815, Truncation lag parameter = 1, p-value = 0.1
 
 #Now, let's evaluate the model using MAPE
 #First, let's make a prediction for the last 6 months
@@ -304,6 +316,280 @@ outdata <- Data_ts[43:48,]
 timevals_out <- outdata$Month
 global_pred_out <- predict(lmfit,data.frame(Month =timevals_out))
 fcast <- global_pred_out
+
+#Now, let's compare our prediction with the actual values, using MAPE
+MAPE_class_dec <- forecast::accuracy(fcast,outdata$Value)[5]
+MAPE_class_dec
+#22.40313
+
+#Let's also plot the predictions along with original values, to
+#get a visual feel of the fit
+class_dec_pred <- c(ts(global_pred),ts(global_pred_out))
+plot(total_timeser, col = "black")
+lines(class_dec_pred, col = "red")
+
+# Forecasting future months
+forecast.decompose <- predict(lmfit,data.frame(Month =49:54))
+
+######## AUTO ARIMA ########
+#So, that was classical decomposition, now let's do an ARIMA fit
+autoarima <- auto.arima(timeser)
+autoarima
+# ARIMA(0,1,0) 
+# 
+# sigma^2 estimated as 25366:  log likelihood=-266.07
+# AIC=534.14   AICc=534.24   BIC=535.85
+
+#Lower the AIC and BIC value, better the model
+#Log likelihood must be higher for better model
+tsdiag(autoarima)
+plot(autoarima$x, col="black")
+lines(fitted(autoarima), col="red")
+
+#Again, let's check if the residual series is white noise
+resi_auto_arima <- timeser - fitted(autoarima)
+adf.test(resi_auto_arima,alternative = "stationary")
+#Dickey-Fuller = -4.3326, Lag order = 3, p-value = 0.01
+kpss.test(resi_auto_arima)
+#KPSS Level = 0.031535, Truncation lag parameter = 1, p-value = 0.1
+
+#Also, let's evaluate the model using MAPE
+fcast_auto_arima <- predict(autoarima, n.ahead = 6)
+MAPE_auto_arima <- accuracy(fcast_auto_arima$pred,outdata$Value)[5]
+MAPE_auto_arima
+#26.24458
+
+#Lastly, let's plot the predictions along with original values, to
+#get a visual feel of the fit
+auto_arima_pred <- c(fitted(autoarima),ts(fcast_auto_arima$pred))
+plot(total_timeser, col = "black")
+lines(auto_arima_pred, col = "purple")
+plot(forecast(auto_arima_pred, h=12))
+# 95% confidence interval is very wide, we can't be sure of the prdictions made.
+
+
+########## Startiing analysis on EU ###########
+#For Forecasting Revenue (Sales) for APAC Consumer Segment
+Data_ts <- Data_EC_Sales
+names(Data_ts) <- c("Month", "Value")
+
+#Counting the number of records/months
+nrow(Data_ts)
+
+total_timeser <- ts(Data_ts$Value)
+plot(total_timeser)
+#We can clearly see from the plot that there is a global trend, no seasonality observed.
+
+indata <- Data_ts[1:42,]
+timeser <- ts(indata$Value)
+plot(timeser)
+
+#Smoothing the series - Moving Average Smoothing
+w <- 1
+smoothedseries <- stats::filter(timeser, 
+                                filter=rep(1/(2*w+1),(2*w+1)), 
+                                method='convolution', sides=2)
+#Smoothing left end of the time series
+diff <- smoothedseries[w+2] - smoothedseries[w+1]
+for (i in seq(w,1,-1)) {
+  smoothedseries[i] <- smoothedseries[i+1] - diff
+}
+#Smoothing right end of the time series
+n <- length(timeser)
+diff <- smoothedseries[n-w] - smoothedseries[n-w-1]
+for (i in seq(n-w+1, n)) {
+  smoothedseries[i] <- smoothedseries[i-1] + diff
+}
+#Plot the smoothed time series
+timevals_in <- indata$Month
+lines(smoothedseries, col="blue", lwd=2)
+
+######## CLASSICAL DECOMPOSITION ########
+#Building a model on the smoothed time series using classical decomposition
+#First, let's convert the time series to a dataframe
+smootheddf <- as.data.frame(cbind(timevals_in, as.vector(smoothedseries)))
+colnames(smootheddf) <- c('Month', 'Value')
+
+#Now, let's fit a additive model with trend and seasonality to the data
+#Seasonality will be modeled using a sinusoid function
+lmfit <- lm(Value ~ sin(0.5*Month) + Month, data=smootheddf)
+global_pred <- predict(lmfit, Month=timevals_in)
+#summary(global_pred)
+#lines(timevals_in, global_pred, col='red', lwd=2)
+lines(global_pred, col='red', lwd=2)
+
+#Now, let's look at the locally predictable series
+#We will model it as an ARMA series
+local_pred <- timeser-global_pred
+plot(local_pred, col='red', type = "l")
+acf(local_pred)
+acf(local_pred, type="partial")
+armafit <- auto.arima(local_pred)
+
+tsdiag(armafit)
+armafit
+# Series: local_pred 
+# ARIMA(0,0,0) with zero mean 
+# 
+# sigma^2 estimated as 113342644:  log likelihood=-449.06
+# AIC=900.12   AICc=900.22   BIC=901.86
+# 
+#
+
+#We'll check if the residual series is white noise
+resi <- local_pred-fitted(armafit)
+adf.test(resi,alternative = "stationary")
+#Dickey-Fuller = -3.9759, Lag order = 3, p-value = 0.02017
+#alternative hypothesis: stationary
+kpss.test(resi)
+#KPSS Level = 0.088036, Truncation lag parameter = 1, p-value = 0.1
+
+#Now, let's evaluate the model using MAPE
+#First, let's make a prediction for the last 6 months
+outdata <- Data_ts[43:48,]
+timevals_out <- outdata$Month
+global_pred_out <- predict(lmfit,data.frame(Month =timevals_out))
+fcast <- global_pred_out
+
+#Now, let's compare our prediction with the actual values, using MAPE
+MAPE_class_dec <- forecast::accuracy(fcast,outdata$Value)[5]
+MAPE_class_dec
+#24.8979
+
+#Let's also plot the predictions along with original values, to
+#get a visual feel of the fit
+class_dec_pred <- c(ts(global_pred),ts(global_pred_out))
+plot(total_timeser, col = "black")
+lines(class_dec_pred, col = "red")
+
+# Forecasting future months Using Classical Decomposition
+forecast.decompose <- predict(lmfit,data.frame(Month =49:54))
+
+######## AUTO ARIMA ########
+#So, that was classical decomposition, now let's do an ARIMA fit
+autoarima <- auto.arima(timeser)
+autoarima
+# ARIMA(0,1,1) 
+# 
+# Coefficients:
+#           ma1
+#       -0.7559
+# s.e.   0.1381
+# 
+# sigma^2 estimated as 174361555:  log likelihood=-447.11
+# AIC=898.23   AICc=898.55   BIC=901.66
+
+#Lower the AIC and BIC value, better the model
+#Log likelihood must be higher for better model
+tsdiag(autoarima)
+plot(autoarima$x, col="black")
+lines(fitted(autoarima), col="red")
+
+#Again, let's check if the residual series is white noise
+resi_auto_arima <- timeser - fitted(autoarima)
+adf.test(resi_auto_arima,alternative = "stationary")
+#Dickey-Fuller = -4.3522, Lag order = 3, p-value = 0.01
+kpss.test(resi_auto_arima)
+#KPSS Level = 0.05314, Truncation lag parameter = 1, p-value = 0.1
+
+#Also, let's evaluate the model using MAPE
+fcast_auto_arima <- predict(autoarima, n.ahead = 6)
+MAPE_auto_arima <- accuracy(fcast_auto_arima$pred,outdata$Value)[5]
+MAPE_auto_arima
+#28.9226
+
+#Lastly, let's plot the predictions along with original values, to
+#get a visual feel of the fit
+auto_arima_pred <- c(fitted(autoarima),ts(fcast_auto_arima$pred))
+plot(total_timeser, col = "black")
+lines(auto_arima_pred, col = "purple")
+plot(forecast(auto_arima_pred, h=12))
+# 95% confidence interval is very wide, we can't be sure of the prdictions made.
+
+
+
+
+####### EU Demand Modeling #######
+#For Forecasting Demand(Quantity) for EU Consumer Segment
+Data_ts <- Data_EC_Quantity
+names(Data_ts) <- c("Month", "Value")
+
+#Counting the number of records/months
+nrow(Data_ts)
+
+total_timeser <- ts(Data_ts$Value)
+plot(total_timeser)
+#We can clearly see from the plot that there is a global trend, no seasonality observed.
+
+indata <- Data_ts[1:42,]
+timeser <- ts(indata$Value)
+plot(timeser)
+
+#Smoothing the series - Moving Average Smoothing
+w <- 1
+smoothedseries <- stats::filter(timeser, 
+                                filter=rep(1/(2*w+1),(2*w+1)), 
+                                method='convolution', sides=2)
+#Smoothing left end of the time series
+diff <- smoothedseries[w+2] - smoothedseries[w+1]
+for (i in seq(w,1,-1)) {
+  smoothedseries[i] <- smoothedseries[i+1] - diff
+}
+#Smoothing right end of the time series
+n <- length(timeser)
+diff <- smoothedseries[n-w] - smoothedseries[n-w-1]
+for (i in seq(n-w+1, n)) {
+  smoothedseries[i] <- smoothedseries[i-1] + diff
+}
+#Plot the smoothed time series
+timevals_in <- indata$Month
+lines(smoothedseries, col="blue", lwd=2)
+######## CLASSICAL DECOMPOSITION ########
+#Building a model on the smoothed time series using classical decomposition
+#First, let's convert the time series to a dataframe
+smootheddf <- as.data.frame(cbind(timevals_in, as.vector(smoothedseries)))
+colnames(smootheddf) <- c('Month', 'Value')
+
+#Now, let's fit a additive model with trend and seasonality to the data
+#Seasonality will be modeled using a sinusoid function
+lmfit <- lm(Value ~ sin(0.5*Month) + Month, data=smootheddf)
+global_pred <- predict(lmfit, Month=timevals_in)
+#summary(global_pred)
+#lines(timevals_in, global_pred, col='red', lwd=2)
+lines(global_pred, col='red', lwd=2)
+
+#Now, let's look at the locally predictable series
+#We will model it as an ARMA series
+local_pred <- timeser-global_pred
+plot(local_pred, col='red', type = "l")
+acf(local_pred)
+acf(local_pred, type="partial")
+armafit <- auto.arima(local_pred)
+
+tsdiag(armafit)
+armafit
+# Series: local_pred 
+# ARIMA(1,0,2) with non-zero mean 
+# 
+# sigma^2 estimated as 11728:  log likelihood=-254.76
+# AIC=519.52   AICc=521.19   BIC=528.21
+# 
+#
+
+#We'll check if the residual series is white noise
+resi <- local_pred-fitted(armafit)
+adf.test(resi,alternative = "stationary")
+#Dickey-Fuller = -2.7939, Lag order = 3, p-value = 0.2607
+#alternative hypothesis: stationary
+kpss.test(resi)
+#KPSS Level = 0.14018, Truncation lag parameter = 1, p-value = 0.1
+
+#Now, let's evaluate the model using MAPE
+#First, let's make a prediction for the last 6 months
+outdata <- Data_ts[43:48,]
+timevals_out <- outdata$Month
+global_pred_out <- predict(lmfit,data.frame(Month =timevals_out))
+fcast <- global_pred_out + predict(armafit, n.ahead=6)$pred
 
 #Now, let's compare our prediction with the actual values, using MAPE
 MAPE_class_dec <- forecast::accuracy(fcast,outdata$Value)[5]
@@ -318,7 +604,6 @@ lines(class_dec_pred, col = "red")
 
 # Forecasting future months
 forecast.decompose <- predict(lmfit,data.frame(Month =49:54))
-line(forecast.decompose)
 
 ######## AUTO ARIMA ########
 #So, that was classical decomposition, now let's do an ARIMA fit
